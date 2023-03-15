0e736ab1d7a5ed9be6f950db80fdf3b8031bc575
David Alejandro Salgado Zelaya <davidsalgado849@gmail.com>
2023-03-14 18:59:26 -0600

    Cambios hechos libs, models y dao

---
+++ src/routes/Music/music.ts
@@ -0,0 +1,70 @@
+import express from 'express';
+const router = express.Router();
+import { MusicDao } from '@server/dao/models/Music/MusicDao';
+import { MongoDBConn } from '@server/dao/MongoDBConn';
+import { IMusic } from '@server/dao/models/Music/IMusic';
+import { Music} from '@libs/Music/Music'
+
+const musicdao = new MusicDao(MongoDBConn);
+let musicModel:Music;
+musicdao.init().then(()=>{
+  musicModel = new Music(musicdao);
+});
+
+
+router.get('/', (_req, res) => {
+    const jsonUrls = {
+        "getAll": { "method": "get", "url": "empresas/all" },
+        "getById": { "method": "get", "url": "empresas/byid/:id" },
+        "new": { "method": "post", "url": "empresas/new" },
+        "update": { "method": "put", "url": "empresas/upd/:id" },
+        "delete": { "method": "delete", "url": "empresas/del/:id" }
+    };
+    res.status(200).json(jsonUrls);
+});
+
+router.get('/all', async(_req, res) => {
+    res.status(200).json(await musicModel.getAllMusic());
+});
+
+router.get('/byid/:id' ,async(req, res)=>{
+    const {id:codigo}= req.params;
+    const empresa = await musicModel.getMusicbyId(codigo);
+    if(empresa){
+        return res.status(200).json(empresa);
+    }
+    return res.status(404).json({"error":"No se encontró empresa"});
+});
+
+router.post('/new', async(req, res) => {
+    console.log("Music /new request body:", req.body);
+    const {
+        codigo="NA",
+        nombre = "John Doe Corp",
+        status = "Activo"
+    } = req.body;
+    //Validar
+    const newMusic: IMusic = {
+        codigo,
+        nombre,
+        status
+    };
+    if (await musicModel.newMusic(newMusic)) {
+        return res.status(200).json({ "created": true });
+    }
+    return res.status(404).json(
+        { "error": "Error al agregar una empresa" }
+    );
+});
+
+
+
+router.delete('/del/:id',async(req, res)=>{
+    const {id} = req.params;
+    if(await musicModel.deleteMusic(id)){
+        return res.status(200).json({"deleted":true });
+    }
+    return res.status(404).json({"error": "no se pudo eliminar empresas"});
+});
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 const router  = express.Router();
+import musicRouter from './Music/music'
 
 router.get('/', (_req, res) => {
   res.json({msg: 'Examen 2ndo Parcial'});
 });
+
+router.use('/music', musicRouter);
 
 export default router;
