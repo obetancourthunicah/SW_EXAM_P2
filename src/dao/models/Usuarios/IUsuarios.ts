43ee5e6e36efe55667f2e96daa06cc190241c388
JoseReyes1504 <eduardo.reyessuazo@gmail.com>
2023-03-14 18:26:50 -0600

    Implementación Interfaz Usuario y DAO

---
+++ src/dao/models/Usuarios/IUsuarios.ts
@@ -0,0 +1,10 @@
+export interface IUsuarios{        
+    nombre:string, 
+    correo:string, 
+    password: string,
+    Foto?: any,
+    Biografia? : string,
+    Informacion_Contacto? : string,
+    Notificacion? : boolean,
+    Configuracion_Privacidad? : string,
+}
