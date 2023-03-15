92d60e36dba51b44ffb522f9cb529dbbd8433bf8
Alejandro Zuniga <84946172+ajzuniga0512@users.noreply.github.com>
2023-03-14 18:44:27 -0600

    Rutas
    se programo la parte de rutas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,10 @@ router.get('/', (_req, res) => {
   res.json({msg: 'Examen 2ndo Parcial'});
 });
 
+import videosRouter from './videos/videos';
+
+
+router.use('/videos', videosRouter);
+
+
 export default router;
---
+++ src/routes/videos/videos.ts
@@ -0,0 +1,95 @@
+import express from 'express';
+const router = express.Router();
+import { EmpresasDao } from '@dao/models/Videos/VideosDao';
+import { MongoDBConn } from '@dao/MongoDBConn';
+import { IVideos } from '@dao/models/Videos/IVideos';
+import { Videos } from '@libs/Videos/Videos';
+const videosDao = new EmpresasDao(MongoDBConn);
+let videosModel:Videos;
+videosDao.init().then(()=>{
+  videosModel = new Videos(videosDao);
+});
+
+
+
+router.get('/all', async (_req, res) => {
+  res.status(200).json(await videosModel.getAll());
+});
+
+router.get('/byid/:id', async (req, res)=>{
+  const {id: codigo} = req.params;
+  const empresa = await videosModel.getById(codigo);
+  if(empresa){
+    return res.status(200).json(empresa);
+  }
+  return res.status(404).json({"error":"No se encontró Empresa"});
+});
+
+router.post('/new', async (req, res) => {
+  console.log("Videos /new request body:", req.body);
+  const {
+    nombre ="Video Github",
+    descripcion = "Como instalar Github",
+    url = "https://www.youtube.com/watch?v=tn6tloweTUs"
+
+
+  } = req.body;
+  
+  const newVideo: IVideos = {
+      nombre,
+      descripcion,
+      url
+  };
+  if (await videosModel.add(newVideo)) {
+    return res.status(200).json({"created": true});
+  }
+  return res.status(404).json(
+    {"error": "Error al agregar un nuevo Video"}
+  );
+});
+
+router.put('/upd/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    nombre="----NoseRecibio------",
+    descripcion="----NoseRecibio------",
+    url = "----NoseRecibio------"
+    
+  } = req.body;
+
+  if (
+    nombre === "----NoseRecibio------"
+    || descripcion === "----NoseRecibio------" || url === "----NoseRecibio------"
+  ) {
+    return res.status(403).json({"error":"Debe venir el nombre, descripcion y url correctos"});
+  }
+  const UpdateVideos : IVideos = {
+    nombre,
+    descripcion,
+    url
+  };
+
+  if (await videosModel.update(id, UpdateVideos)) {
+    return res
+      .status(200)
+      .json({"updated": true});
+  }
+  return res
+    .status(404)
+    .json(
+      {
+        "error": "Error al actualizar el video"
+      }
+    );
+});
+
+router.delete('/del/:id', async (req, res)=>{
+  const {id } = req.params;
+  if(await videosModel.delete(id)){
+    return res.status(200).json({"deleted": true});
+  }
+  return res.status(404).json({"error":"No se pudo eliminar el video"});
+});
+
+
+export default router;
