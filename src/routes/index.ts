34dda104cb88b134f29c842ae0d2c4d1e2ee4ee4
Sofía Ramirez <sofiaraquel109@hotmail.com>
2023-03-14 19:00:36 -0600

    ruta en archivo index.ts en carpeta routes
    colocada ruta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,7 @@ router.get('/', (_req, res) => {
   res.json({msg: 'Examen 2ndo Parcial'});
 });
 
+import plantasRoute from './plantas/Plantas';
+router.use('/Plantas', plantasRoute);
+
 export default router;
